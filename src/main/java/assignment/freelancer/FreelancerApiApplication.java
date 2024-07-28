3320790d526cf3576a95f356b9245563c62ec81e
kimsan <cloudhat17@gmail.com>
2024-07-28 17:13:44 +0900

    init

---
+++ src/main/java/assignment/freelancer/FreelancerApiApplication.java
@@ -0,0 +1,13 @@
+package assignment.freelancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelancerApiApplication.class, args);
+    }
+
+}
